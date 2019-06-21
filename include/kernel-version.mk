384bc43a57d31dec0f02dbaee41af415bc30e49d
John Crispin <john@phrozen.org>
2019-06-21 08:08:48 +0200

    prpl: Partially revert "kernel: drop everything not on kernel version 4.14"
    Only revert the generic patches part.
    
    This reverts commit 000d400baa0af2e42c9a462e42df7dc9abde1ec7.

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -7,8 +7,10 @@ ifdef CONFIG_TESTING_KERNEL
 endif
 
 LINUX_VERSION-4.14 = .180
+LINUX_VERSION-4.19 = .101
 
 LINUX_KERNEL_HASH-4.14.180 = 444ef973d9b6a6ea174e4a9086f0aea980d8575d13302e431ad688f22e27ed0e
+LINUX_KERNEL_HASH-4.19.101 = be26156abdb38ac0576a34a235ef456bb8ca67fbbe56fc6649b8d069159f8bc4
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
