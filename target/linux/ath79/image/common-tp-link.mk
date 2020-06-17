7c13715d9752f7061c42649ee3f75e362db4da79
Petr Štetiar <ynezz@true.cz>
2020-06-17 23:48:32 +0200

    ath79: image: fix initramfs for safeloader devices
    Currently it's not possible to tftpboot initramfs image on archer-c7-v5
    as the image contains tplink-v1-header which leads to:
    
     ath> bootm
     ## Booting image at 81000000 ...
     Bad Magic Number
    
    as U-Boot expects uImage wrapped image. This is caused by following
    inheritance issue:
    
      define Device/Init
        KERNEL_INITRAMFS = $$(KERNEL)
    
      define Device/tplink-v1
        KERNEL := kernel-bin | append-dtb | lzma
        KERNEL_INITRAMFS := kernel-bin | append-dtb | lzma | tplink-v1-header
    
      define Device/tplink-safeloader
        $(Device/tplink-v1)
    
      define Device/tplink-safeloader-uimage
        $(Device/tplink-safeloader)
        KERNEL := kernel-bin | append-dtb | lzma | uImageArcher lzma
    
      define Device/tplink_archer-c7-v5
        $(Device/tplink-safeloader-uimage)
    
    where tplink-v1 defines KERNEL_INITRAMFS with tplink-v1-header and it's
    then used by all devices inheriting from tplink-safeloader. Fix this by
    overriding KERNEL_INITRAMFS to KERNEL variable again.
    
    Signed-off-by: Petr Štetiar <ynezz@true.cz>
    (cherry picked from commit ceeece9ffaa5a3a336505332c39794d76c08b2ca)

--- target/linux/ath79/image/common-tp-link.mk
+++ target/linux/ath79/image/common-tp-link.mk
@@ -100,6 +100,7 @@ endef
 define Device/tplink-safeloader
   $(Device/tplink)
   KERNEL := kernel-bin | append-dtb | lzma | tplink-v1-header -O
+  KERNEL_INITRAMFS := $$(KERNEL)
   IMAGE/sysupgrade.bin := append-rootfs | tplink-safeloader sysupgrade | \
     append-metadata | check-size $$$$(IMAGE_SIZE)
   IMAGE/factory.bin := append-rootfs | tplink-safeloader factory
@@ -108,4 +109,5 @@ endef
 define Device/tplink-safeloader-uimage
   $(Device/tplink-safeloader)
   KERNEL := kernel-bin | append-dtb | lzma | uImageArcher lzma
+  KERNEL_INITRAMFS := $$(KERNEL)
 endef
