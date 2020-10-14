8edf07e3b6bdfd1ba19e942fbca42f4b6c8c561b
Petr Štetiar <ynezz@true.cz>
2020-10-14 16:19:03 +0200

    scripts/prpl-jira.py: improve build failure reporting
    * try to parse build logs and provide more details about build failures
      directly into the issue
    * attach build logs for failed components directly to the issue
    * add `--dry-run` switch so it's possible to see what would be done on Jira
    
    Signed-off-by: Petr Štetiar <ynezz@true.cz>

--- scripts/gitlab/prpl-jira.py
+++ scripts/gitlab/prpl-jira.py
@@ -1,11 +1,85 @@
 #!/usr/bin/env python3
 
+import re
 import sys
 import os
+import glob
 import logging
 import argparse
 
 from jira import JIRA
+from dataclasses import dataclass
+
+
+@dataclass
+class BuildLogFailure:
+    path: str
+    step: str
+    name: str
+    tail_log: list
+
+
+class BuildLogAnalyzer:
+    # make[3]: *** [Makefile:154: /builds/swpal_6x-uci-1.0.0.1.tar.bz2] Error 128
+    RE_MAKE_ERROR = re.compile(r"make\[\d+\]: \*\*\* (.*) Error")
+
+    # Makefile:563: recipe for target 'lldpcli' failed
+    RE_MAKE_RECIPE_FAILED = re.compile(
+        r"^Makefile:\d+: recipe for target '(.*)' failed"
+    )
+
+    def __init__(self, log_dir):
+        self.log_dir = log_dir
+        self._failures = []
+        self.analyze_logs()
+
+    def get_tail_log(self, path):
+        tail_log = []
+        re_matchers = [self.RE_MAKE_ERROR, self.RE_MAKE_RECIPE_FAILED]
+
+        def add_tail_log(line):
+            tail_log_goal = 4
+            tail_log.append(line)
+            truncate = len(tail_log) - tail_log_goal
+            if truncate > 0:
+                return tail_log[truncate:]
+            return tail_log
+
+        with open(path, "r") as fd:
+            for line in fd:
+                line = line.rstrip()
+                if len(line) == 0:
+                    continue
+
+                tail_log = add_tail_log(line)
+
+                for matcher in re_matchers:
+                    if matcher.match(line):
+                        return tail_log
+
+    def process_log_file(self, path):
+        path = os.path.normpath(path)
+        filename = os.path.basename(path)
+        dirname, package = os.path.split(path)
+        (step, _) = os.path.splitext(filename)
+
+        if step == "check-compile":
+            return
+
+        tail_log = self.get_tail_log(path)
+        if not tail_log:
+            return
+
+        self._failures.append(BuildLogFailure(path, step, package, tail_log))
+
+    def analyze_logs(self):
+        glob_pattern = os.path.join(self.log_dir, "**/**.txt")
+        for item in glob.iglob(glob_pattern, recursive=True):
+            if os.path.isfile(item):
+                self.process_log_file(item)
+
+    def failures(self):
+        return self._failures
 
 
 class JiraHelper:
@@ -29,7 +103,40 @@ class JiraHelper:
 
         self.jira = JIRA({"server": args.instance_url}, oauth=oauth_dict)
 
+    def get_build_failure_string(self):
+        if not self.build_failures:
+            return ""
+
+        r = "\nSeems like there are build issues with following items:\n"
+        for f in self.build_failures:
+            r += f" * {f.name} ({f.step}):\n"
+            for line in f.tail_log:
+                r += f" ** {line}\n"
+            r += "\n"
+
+        return r
+
     def build_failure(self):
+        commit = os.getenv("CI_COMMIT_SHORT_SHA", "commit_sha")
+        self.build_failures = BuildLogAnalyzer(self.args.build_logs_dir).failures()
+
+        issue = self.build_failure_issue()
+        if not issue or not self.build_failures:
+            return
+
+        for failure in self.build_failures:
+            filename = f"{failure.name}_{failure.step}_{commit}.log"
+
+            if self.args.dry_run:
+                logging.info(f"Would add attachment {filename} from {failure.path})")
+                continue
+
+            logging.info(f"Adding attachment {filename} from {failure.path})")
+            self.jira.add_attachment(
+                issue=issue, attachment=failure.path, filename=filename
+            )
+
+    def build_failure_issue(self):
         args = self.args
         job = os.getenv("CI_JOB_NAME", "job")
         project = os.getenv("CI_PROJECT_NAME", "project")
@@ -38,16 +145,18 @@ class JiraHelper:
         job_url = os.getenv("CI_JOB_URL", "https://job_url")
         commit = os.getenv("CI_COMMIT_SHORT_SHA", "commit_sha")
         commit_message = os.getenv("CI_COMMIT_MESSAGE", "foor bar baz")
+        build_failures = self.get_build_failure_string()
 
         summary = f"CI build failure in {project}/{branch} during {job}"
 
         description = (
             f"Just noticed build failure during execution of "
-            f"[{job}|{job_url}] job in _{project}/{branch}_ which is now at "
+            f"[{job}|{job_url}] CI job in _{project}/{branch}_ which is now at "
             f"[{commit}|{project_url}/-/commit/{commit}] commit:"
             "{noformat}"
             f"{commit_message}"
             "{noformat}"
+            f"{build_failures}"
         )
 
         jql = f"""
@@ -59,7 +168,14 @@ class JiraHelper:
             logging.info(
                 f"Updating ({existing_issue.key}) {existing_issue.fields.summary}"
             )
-            self.jira.add_comment(existing_issue, description)
+
+            if not args.dry_run:
+                self.jira.add_comment(existing_issue, description)
+
+            return existing_issue
+
+        if args.dry_run:
+            logging.info(f"Would create new issue: '{self.args.project}/{summary}'")
             return
 
         new_issue = self.jira.create_issue(
@@ -72,6 +188,7 @@ class JiraHelper:
         )
 
         logging.info(f"Created ({new_issue.key}) {new_issue.fields.summary}")
+        return new_issue
 
 
 def main():
@@ -81,6 +198,11 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--debug", action="store_true", help="enable debug mode")
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        help="Do not alter Jira content, just printout what would be done",
+    )
 
     parser.add_argument(
         "--access-token",
@@ -126,6 +248,12 @@ def main():
 
     subparsers = parser.add_subparsers(dest="command", title="available subcommands")
     subparser = subparsers.add_parser("build_failure", help="report build failure")
+    subparser.add_argument(
+        "--build-logs-dir",
+        type=str,
+        default=os.path.join(os.getcwd(), "logs"),
+        help="Path to directory which contains build logs",
+    )
     subparser.set_defaults(func=JiraHelper.build_failure)
 
     args = parser.parse_args()
