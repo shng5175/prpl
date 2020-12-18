openwrt/openwrt

ath79: add support for TP-Link TL-WR802N V1 and V2
Specification:
- SoC: Qualcomm Atheros QCA9533 (560 MHz, MIPS 24Kc)
- RAM: 32 MiB
- Storage: 4 MiB of Flash on board
- Wireless: Built into QCA9533 (Honey Bee), PHY modes b/g/n
- Ethernet: 1x100M (port0)

Installation through OEM Web Interface:
- Connect to TL-WR802N by Ethernet or Wi-Fi
- Go to web interface:
  [V1] http://192.168.0.1
  [V2] http://192.168.0.254
  Default user is "admin" & password is "admin".
  On V2, there is no DHCP server running by default, so remember to set
  IP manually.
- Go to "System Tools -> Firmware Upgrade"
- Browse for firmware:
  [V1] "*.factory.bin"
  [V2] "*.factory-us.bin" or  "*.factory-eu.bin" for eu model
  Web interface may complain if filename is too long. In such case,
  rename .bin to something shorter.
- Click upgrade

Installation through tftp:
Note: T_OUT, T_IN and GND on the board must be connected to USB TTL
      Serial Configuration 115200 8n1

- Boot the TL-WR802N
- When "Autobooting in 1 seconds" appears type "tpl" followed by enter
- Connect to the board Ethernet port
    (IPADDR: 192.168.1.1, ServerIP: 192.168.1.10)
- tftpboot 0x80000000 <Firmware Image Name>
- Record the result of "printenv bootcmd"
- Enter "erase <Result of 'printenv bootcmd'> +0x3c0000"
    (e.g erase 0x9f020000 +0x3c0000)
- Enter "cp.b 0x80000000 <Result of 'printenv bootcmd'> 0x3c0000"
    (e.g cp.b 0x80000000 0x9f020000 0x3c0000)
- Enter "bootm <Result of 'printenv bootcmd'>"
    (e.g bootm 0x9f020000)

When porting from ar71xx target to ath79, I found out that on V2,
reset button is on GPIO12 and active low, instead of GPIO11 and
acticve high. By cross-flashing V1 firmware to V2, I confirmed
the same is true for V1.
Also according to manual of V1, this one also has green
LED instead of blue - both of those issues were fixed accordingly.

Installation manual based on ar71xx support by Thomas Roberts

Signed-off-by: Lech Perczak <lech.perczak@gmail.com>

target/linux/ath79/image/tiny-tp-link.mk
@@ -272,6 +272,30 @@ define Device/tplink_tl-wr743nd-v1
 endef
 TARGET_DEVICES += tplink_tl-wr743nd-v1
 
+define Device/tplink_tl-wr802n-v1
+  $(Device/tplink-4mlzma)
+  SOC := qca9533
+  DEVICE_MODEL := TL-WR802N
+  DEVICE_VARIANT := v1
+  TPLINK_HWID := 0x08020001
+  SUPPORTED_DEVICES += tl-wr802n-v1
+endef
+TARGET_DEVICES += tplink_tl-wr802n-v1
+
+define Device/tplink_tl-wr802n-v2
+  $(Device/tplink-4mlzma)
+  SOC := qca9533
+  DEVICE_MODEL := TL-WR802N
+  DEVICE_VARIANT := v2
+  TPLINK_HWID := 0x08020002
+  SUPPORTED_DEVICES += tl-wr802n-v2
+  TPLINK_HWREV := 2
+  IMAGES += factory-us.bin factory-eu.bin
+  IMAGE/factory-us.bin := tplink-v1-image factory -C US
+  IMAGE/factory-eu.bin := tplink-v1-image factory -C EU
+endef
+TARGET_DEVICES += tplink_tl-wr802n-v2
+
 define Device/tplink_tl-wr841-v5
   $(Device/tplink-4m)
   SOC := ar7240

shng5175/prpl

Notes:
active high. By cross-flashing V1 firmware to V2, I confirmed
The MAC address assignment has been checked with OEM firmware.
[slightly adjust commit message, add MAC address comment]
Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>
(cherry picked from commit 7e513136c63108bf55b38be4d2b65aa00b0d2b26)

target/linux/ath79/image/tiny-tp-link.mk
@@ -227,6 +227,28 @@ define Device/tplink_tl-wr743nd-v1
 endef
 TARGET_DEVICES += tplink_tl-wr743nd-v1
 
+define Device/tplink_tl-wr802n-v1
+  $(Device/tplink-4mlzma)
+  ATH_SOC := qca9533
+  DEVICE_TITLE := TP-Link TL-WR802N v1
+  TPLINK_HWID := 0x08020001
+  SUPPORTED_DEVICES += tl-wr802n-v1
+endef
+TARGET_DEVICES += tplink_tl-wr802n-v1
+
+define Device/tplink_tl-wr802n-v2
+  $(Device/tplink-4mlzma)
+  ATH_SOC := qca9533
+  DEVICE_TITLE := TP-Link TL-WR802N v2
+  TPLINK_HWID := 0x08020002
+  TPLINK_HWREV := 2
+  SUPPORTED_DEVICES += tl-wr802n-v2
+  IMAGES += factory-us.bin factory-eu.bin
+  IMAGE/factory-us.bin := append-rootfs | mktplinkfw factory -C US
+  IMAGE/factory-eu.bin := append-rootfs | mktplinkfw factory -C EU
+endef
+TARGET_DEVICES += tplink_tl-wr802n-v2
+
 define Device/tplink_tl-wr841-v5
   $(Device/tplink-4m)
   ATH_SOC := ar7240